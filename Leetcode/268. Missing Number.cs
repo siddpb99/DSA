ea62347e6bf5762aee5b12cea4fbea1210ac78cf
Siddpb99 <siddharth.wt.sr@gmail.com>
2025-09-04 12:49:57 +0530

    268. Missing Number

---
+++ Leetcode/268. Missing Number.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length; int res = 0; int sum = 0; int sumnums = 0;//isme sum me pura no ka sum 
+        for (int i = 0; i < n; i++)//aur sumnums me jo v nums k andar uska sum fir subtract krnepe missing
+        {
+
+            sumnums = sumnums + nums[i];
+        }
+        for (int i = 0; i <= n; i++)
+        {
+
+            sum = sum + i;
+        }
+        res = sum - sumnums;
+        return res;
+    }
+}
