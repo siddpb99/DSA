8e5531312c27f7e5720cfb395693fa28ce07dee8
Siddharth Lakshmi <siddharth_lakshmi@epam.com>
2026-04-14 11:40:18 +0530

    Add explanatory comments to valid parentheses solution

--- Leetcode/20.Valid PAranthesis.cs
+++ Leetcode/20.Valid PAranthesis.cs
@@ -16,28 +16,35 @@ namespace ValidParenthesesApp
 
 		public static bool IsValid(string s)
 		{
+			// Use a stack to keep track of opening brackets while scanning the string.
 			Stack<char> myStack = new Stack<char>();
 
 			foreach (char c in s)
 			{
+				// If the character is an opening bracket, push it onto the stack.
 				if (c == '(' || c == '[' || c == '{')
 				{
 					myStack.Push(c);
 				}
 				else
 				{
+					// If the current character is a closing bracket but the stack is empty,
+					// there is no matching opening bracket.
 					if (myStack.Count == 0) return false;
 
+					// Pop the most recent opening bracket and compare it with the closing one.
 					char top = myStack.Pop();
 					if ((c == ')' && top != '(') ||
 						(c == ']' && top != '[') ||
 						(c == '}' && top != '{'))
 					{
+						// The closing bracket does not match the opening bracket.
 						return false;
 					}
 				}
 			}
 
+			// The string is valid only if there are no unmatched opening brackets left.
 			return myStack.Count == 0;
 		}
 	}
